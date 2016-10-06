6bf1eea17f8794e6b34b84b82b8c240b46015559
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2016-10-06 16:57:59 +0100

    arkivum2 ready for epm

--- cfg/cfg.d/10_core.pl
+++
@@ -1,8 +0,0 @@
-# This file was created by bin/epadmin
-# You can regenerate this file by doing ./bin/epadmin config_core template
-$c->{host} = 'w01.k2.da.ulcc.ac.uk';
-$c->{port} = 80;
-$c->{aliases} = [];
-$c->{securehost} = '';
-$c->{secureport} = 443;
-$c->{http_root} = undef;
--- cfg/cfg.d/database.pl
+++
@@ -1,7 +0,0 @@
-$c->{dbname} = 'k2';
-$c->{dbhost} = 'localhost';
-$c->{dbport} = undef;
-$c->{dbsock} = undef;
-$c->{dbuser} = 'k2';
-$c->{dbpass} = '3CWdlmu1';
-$c->{dbengine} = 'MyISAM';
