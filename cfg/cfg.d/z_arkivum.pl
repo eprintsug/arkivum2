c6b371d88f9a637ac0297bf81547d82196c03aed
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2016-10-08 10:20:35 +0100

    sensible parameter hints

--- cfg/cfg.d/z_arkivum.pl
+++ cfg/cfg.d/z_arkivum.pl
@@ -2,10 +2,10 @@
 my $arkivum = {};
 $c->{arkivum} = $arkivum;
 
-$arkivum->{archive_api} = "https://82.144.240.138:8443";
-$arkivum->{file_share_host} = "82.144.240.135/owncloud";
-$arkivum->{file_share_user} = "astoradmin";
-$arkivum->{file_share_password} = "arkivum";
+$arkivum->{archive_api} = "ADDRESS_OF_ASTOR_DEVICE"; # eg https://astor.institution.ac.uk:8443";
+$arkivum->{file_share_host} = "ADDRESS_OF_OWNCLOUD_SERVICE"; #eg owncloud.institution.ac.uk/owncloud
+$arkivum->{file_share_user} = "ARKIVUM_USER";
+$arkivum->{file_share_password} = "ARKIVUM_PASSWORD";
 
 $arkivum->{default_document_security} = "public";
 $arkivum->{default_document_license} = "cc_by_nd";
@@ -16,10 +16,10 @@ $arkivum->{checksumAlgorithm} = "md5";
 
 #What Owncloud calls it
 #This is the folder name of the external storage location (type==local) configured in on the owncloud external storage app
-$arkivum->{ext_storage_name} = "arkivum"; 
+$arkivum->{ext_storage_name} = "ARKIVUM_DIR_THAT_OWNCLOUD_CAN_SEE"; 
 #What arkivum calls it
 #This is the path from the astor root to the "configurtion" set in the owncloud external storage app
-$arkivum->{file_share_folder} = "owncloud_datapool";
+$arkivum->{file_share_folder} = "ARKIVUM_DIR_OR_DATAPOOL";
 
 
 
